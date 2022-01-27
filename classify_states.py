e66dd2e69d0d71a47d4c41e0517e20b126ddde1a
jwgerlach00 <jwgerlach00@gmail.com>
2022-01-26 20:33:08 -0500

    Implemented peak detection and gradient peak detection for state prediction

--- classify_states.py
+++ classify_states.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from data_clip_fix import FixClip
+from scipy.signal import find_peaks
 
 
 class StateClassifier:
@@ -17,12 +18,8 @@ class StateClassifier:
         
         self.third = self.range/3
         
-        self.states = {
-            0: self.standing_straight,
-            -40: self.forward_swing,
-            50: self.backward_swing
-        }  # degrees
-        print('third', self.third)
+        self.states = {0: self.standing_straight, -40: self.forward_swing, 50: self.backward_swing}  # degrees
+        
     def classify(self):
         for ang, t in zip(self.euler, self.time):
             if ang <= self.third:
@@ -35,7 +32,6 @@ class StateClassifier:
             elif ang >= 2*self.third:
                 self.states[50][0].append(t)
                 self.states[50][1].append(ang)
-        
 
 
 if __name__ == '__main__':
@@ -47,17 +43,41 @@ if __name__ == '__main__':
     # Fix clipping
     new_euler3 = FixClip(euler3).new_euler
     
-    
+    # Scale euler between -1 and 1
     scaler = MinMaxScaler()
     new_euler3 = scaler.fit_transform(np.array(new_euler3).reshape(-1, 1))
     
-    classifier = StateClassifier(new_euler3, time)
     
-    classifier.classify()
+    # Plotting
+    fig, (ax1, ax2) = plt.subplots(2)
+    
+    # Create euler plot
+    ax1.plot(time, new_euler3, label='Euler 3', color='orange')
+    
+    # Create gradient of euler plot
+    grad = np.gradient(np.squeeze(new_euler3))
+    ax2.plot(time, grad, label='Gradient', color='b')
+    
+    # Identify and plot euler peaks
+    peak_indices = find_peaks(np.squeeze(new_euler3))[0]
+    ax1.scatter(time[peak_indices], new_euler3[peak_indices], color='black', label='Peaks')
+    
+    # Identify and plot gradient peaks
+    grad_peak_indices = find_peaks(grad, prominence=.1)[0]
+    ax1.scatter(time[grad_peak_indices], new_euler3[grad_peak_indices])
+    grad_valley_indices = find_peaks(-grad, prominence=.1)[0]
+    ax1.scatter(time[grad_valley_indices], new_euler3[grad_valley_indices])
+    
+    # Plot configs
+    plt.xlabel('time')
+    plt.ylabel('mag')
+    ax1.legend()
+    ax2.legend()
     
     
-    plt.plot(time, new_euler3)
-    plt.scatter(classifier.standing_straight[0], classifier.standing_straight[1])
-    plt.scatter(classifier.forward_swing[0], classifier.forward_swing[1])
-    plt.scatter(classifier.backward_swing[0], classifier.backward_swing[1])
+    # classifier = StateClassifier(new_euler3, time)
+    # classifier.classify()
+    # plt.scatter(classifier.standing_straight[0], classifier.standing_straight[1])
+    # plt.scatter(classifier.forward_swing[0], classifier.forward_swing[1])
+    # plt.scatter(classifier.backward_swing[0], classifier.backward_swing[1])
     plt.show()
